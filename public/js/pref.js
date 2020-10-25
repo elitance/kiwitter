83722ff4bf0360ea388f040cfe4b064673294959
Lucas Han <evannilaz@gmail.com>
2020-10-25 14:17:50 +0900

    Delete: label.js

--- public/js/pref.js
+++ public/js/pref.js
@@ -7,19 +7,22 @@ if (location.hash) {
     document.querySelector('.current').classList.remove('current');
     hashElt.classList.add('current');
     document.querySelector('.bar').style.marginTop = mtArr[catsArr.indexOf(hashElt)];
-    fetch(`/html/${location.hash.slice(1)}.html`).then(async(resp) => {
-        const html = await resp.text();
-        document.querySelector('.load').classList.add('hidden');
-        document.querySelector('main').innerHTML = html;
-    });
+} else {
+    location.hash = 'account';
 }
+
+fetch(`/html/${location.hash.slice(1)}.html`).then(async(resp) => {
+    const html = await resp.text();
+    document.querySelector('.load').classList.add('hidden');
+    document.querySelector('main').innerHTML = html;
+});
 
 prefCats.forEach((prefCat) => {
     prefCat.addEventListener('click', (e) => {
         const mt = mtArr[catsArr.indexOf(e.target)];
         const current = document.querySelector('.current');
         const bar = document.querySelector('.bar');
-        bar.style.marginTop = mt
+        bar.style.marginTop = mt;
         
         current.classList.remove('current');
         e.target.classList.add('current');
